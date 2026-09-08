b0c5b600c64a9ba0a35e2d0070b4c3b372215cbd
syuilo <4439005+syuilo@users.noreply.github.com>
2026-09-08 18:20:08 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { createApp } from 'vue';
 import '@/style.scss';
 import App from '@/App.vue';
-import { createPinia } from 'pinia';
 import directives from '@/directives/index.js';
 import '@tabler/icons-webfont/dist/tabler-icons.scss';
 
 const app = createApp(App);
-const pinia = createPinia();
 
-app.use(pinia);
 directives(app);
 
 app.mount('#app');
